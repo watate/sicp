7e9f805ae086a3c14577c588808c10d0234e5ca2
Walter <walter.spades@gmail.com>
2020-12-28 00:10:47 +0800

    Week 7: Sunday Part 2

---
+++ exercise_01_40.scm
@@ -0,0 +1,63 @@
+;HEADERS
+(define nil '())
+(define true #t)
+(define false #f)
+(define (random n)
+  (random-integer n))
+(define (square x)
+        (* x x))
+(define (cube x)
+        (* x x x))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (even? n)
+    (cond ((= (mod n 2) 0) true)
+    (else false)))
+
+; https://github.com/biwascheme/biwascheme/issues/110#issuecomment-335869546
+(define (date2runtime date)
+  ; HACK
+  ; wraps around occasionally!
+  (+  
+     (* (date-hour date) 60 60 1000) 
+     (* (date-minute date) 60 1000) 
+     (* (date-second date) 1000) 
+     (date-millisecond date)
+  )
+)
+
+(define (runtime) (date2runtime (current-date)))
+
+;MAIN
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+
+(define dx 0.00001)
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+      tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x) 
+    (+ (cube x) 
+       (* a (square x)) 
+       (* b x) 
+       c)))
+
+(newtons-method (cubic 1 2 3) 1) ;x≈-1.2757
---
+++ exercise_01_41.scm
@@ -0,0 +1,39 @@
+;HEADERS
+(define nil '())
+(define true #t)
+(define false #f)
+(define (random n)
+  (random-integer n))
+(define (square x)
+        (* x x))
+(define (cube x)
+        (* x x x))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (even? n)
+    (cond ((= (mod n 2) 0) true)
+    (else false)))
+
+; https://github.com/biwascheme/biwascheme/issues/110#issuecomment-335869546
+(define (date2runtime date)
+  ; HACK
+  ; wraps around occasionally!
+  (+  
+     (* (date-hour date) 60 60 1000) 
+     (* (date-minute date) 60 1000) 
+     (* (date-second date) 1000) 
+     (date-millisecond date)
+  )
+)
+
+(define (runtime) (date2runtime (current-date)))
+
+;MAIN
+(define (double g)
+  (lambda (x) (g (g x))))
+
+(define (inc x)
+  (+ x 1))
+
+((double inc) 5)
+;((double (double inc)) 5)
