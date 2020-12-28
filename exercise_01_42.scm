090faeb0af6c6158339f7bbdffa52f86ff949732
Walter <walter.spades@gmail.com>
2020-12-28 23:56:19 +0800

    Week 8: Monday

---
+++ exercise_01_42.scm
@@ -0,0 +1,38 @@
+;HEADERS
+(define nil '())
+(define true #t)
+(define false #f)
+(define (random n)
+  (random-integer n))
+(define (square x)
+        (* x x))
+(define (cube x)
+        (* x x x))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (even? n)
+    (cond ((= (mod n 2) 0) true)
+    (else false)))
+
+; https://github.com/biwascheme/biwascheme/issues/110#issuecomment-335869546
+(define (date2runtime date)
+  ; HACK
+  ; wraps around occasionally!
+  (+  
+     (* (date-hour date) 60 60 1000) 
+     (* (date-minute date) 60 1000) 
+     (* (date-second date) 1000) 
+     (date-millisecond date)
+  )
+)
+
+(define (runtime) (date2runtime (current-date)))
+
+;MAIN
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (inc x)
+  (+ x 1))
+
+((compose square inc) 6)
---
+++ exercise_01_43.scm
@@ -0,0 +1,42 @@
+;HEADERS
+(define nil '())
+(define true #t)
+(define false #f)
+(define (random n)
+  (random-integer n))
+(define (square x)
+        (* x x))
+(define (cube x)
+        (* x x x))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (even? n)
+    (cond ((= (mod n 2) 0) true)
+    (else false)))
+
+; https://github.com/biwascheme/biwascheme/issues/110#issuecomment-335869546
+(define (date2runtime date)
+  ; HACK
+  ; wraps around occasionally!
+  (+  
+     (* (date-hour date) 60 60 1000) 
+     (* (date-minute date) 60 1000) 
+     (* (date-second date) 1000) 
+     (date-millisecond date)
+  )
+)
+
+(define (runtime) (date2runtime (current-date)))
+
+;MAIN
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (inc x)
+  (+ x 1))
+
+(define (repeated f n)
+  (cond ((= n 1) (lambda (x) (f x)))
+        (else (compose f (repeated f (- n 1))))))
+
+((repeated square 2) 5)
